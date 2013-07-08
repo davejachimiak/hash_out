9e5a5d9b78cd79aeeee91c72b319ccf18b73a34c
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-07 21:52:16 -0400

    Compose methods

--- lib/public_hash.rb
+++ lib/public_hash.rb
@@ -1,35 +1,42 @@
 module PublicHash
+  require 'set'
+
   def public_hash
-    @public_hash || determine_public_hash
+    initialize_exclusions
+    set_hash
+    delete_exclusions
+    @hash
   end
 
   private
 
-  def determine_public_hash
-    @exclusions  = []
-    @public_hash = delete_exclusions Hash[public_methods_and_values]
+  def initialize_exclusions
+    @exclusions ||= Set.new
+  end
+
+  def set_hash
+    @hash = Hash[public_methods_and_values]
+  end
+
+  def delete_exclusions
+    @exclusions.each { |exclusion| @hash.delete exclusion }
   end
 
   def public_methods_and_values
-    public_method_names.map { |method_name| name_value_pair method_name  }
-  end
-
-  def name_value_pair method_name
-    [method_name, send(method_name)]
+    public_method_names.map { |method_name| name_value_pair method_name }
   end
 
   def public_method_names
     public_methods false
   end
 
-  def delete_exclusions hash
-    @exclusions.each { |exclusion| hash.delete exclusion }
-    hash
+  def name_value_pair method_name
+    [method_name, send(method_name)]
   end
 
   def exclude_from_public_hash
     excluded_method = caller_method_sym caller.first
-    @exclusions.push excluded_method
+    @exclusions.add excluded_method
   end
 
   def caller_method_sym caller_string
