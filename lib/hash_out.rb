44cefef36639595f5c1601e62cb6562e4a13fd1a
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-16 11:31:33 -0400

    Extract Hasher to file

--- lib/hash_out.rb
+++ lib/hash_out.rb
@@ -1,4 +1,4 @@
-require 'set'
+require 'hash_out/hasher'
 
 module HashOut
   def hash_out
@@ -32,59 +32,5 @@ module HashOut
 
   def _hasher hash_out_caller=nil
     @_hasher ||= Hasher.new self, hash_out_caller
-  end
-
-  class Hasher < Struct.new :object, :hash_out_caller
-    def object_to_hash
-      exclusions.add hash_out_caller
-
-      block_recursion do
-        set_hash
-        delete_exclusions
-      end
-
-      @hash
-    end
-
-    def exclusions
-      @exclusions ||= Set.new
-    end
-
-    private
-
-    def block_recursion
-      @times_called ||= 0
-      @times_called  += 1
-
-      if @times_called == 1
-        yield
-      else
-        return
-      end
-
-      @times_called = 0
-    end
-
-    def set_hash
-      @hash = Hash[interesting_methods_and_values]
-    end
-
-    def interesting_methods_and_values
-      methods_requiring_no_arguments.map do |method_name|
-        name_value_pair method_name
-      end
-    end
-
-    def methods_requiring_no_arguments
-      object.public_methods(false).select { |m| [-1, 0].include? object.method(m).arity }
-    end
-
-    def name_value_pair method_name
-      [method_name, object.send(method_name)]
-    end
-
-    def delete_exclusions
-      exclusions.each { |exclusion| @hash.delete exclusion }
-    end
   end
 end
---
+++ lib/hash_out/hasher.rb
@@ -0,0 +1,57 @@
+require 'set'
+
+module HashOut
+  class Hasher < Struct.new :object, :hash_out_caller
+    def object_to_hash
+      exclusions.add hash_out_caller
+
+      block_recursion do
+        set_hash
+        delete_exclusions
+      end
+
+      @hash
+    end
+
+    def exclusions
+      @exclusions ||= Set.new
+    end
+
+    private
+
+    def block_recursion
+      @times_called ||= 0
+      @times_called  += 1
+
+      if @times_called == 1
+        yield
+      else
+        return
+      end
+
+      @times_called = 0
+    end
+
+    def set_hash
+      @hash = Hash[interesting_methods_and_values]
+    end
+
+    def interesting_methods_and_values
+      methods_requiring_no_arguments.map do |method_name|
+        name_value_pair method_name
+      end
+    end
+
+    def methods_requiring_no_arguments
+      object.public_methods(false).select { |m| [-1, 0].include? object.method(m).arity }
+    end
+
+    def name_value_pair method_name
+      [method_name, object.send(method_name)]
+    end
+
+    def delete_exclusions
+      exclusions.each { |exclusion| @hash.delete exclusion }
+    end
+  end
+end
