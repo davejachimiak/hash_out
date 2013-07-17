cdf2047514a205176bb1a051d8b5e783074518d6
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-17 09:24:36 -0400

    Extract last call

--- lib/hash_out.rb
+++ lib/hash_out.rb
@@ -1,23 +1,18 @@
+require 'last_call'
 require 'hash_out/hasher'
 
 module HashOut
   def hash_out
-    hash_out_caller = _caller_method_sym caller.first
-    _hasher(hash_out_caller).object_to_hash
+    _hasher(last_call).object_to_hash
   end
 
   protected
 
   def exclude_from_hash_out
-    excluded_method = _caller_method_sym caller.first
-    _exclusions.add excluded_method
+    _exclusions.add last_call
   end
 
   private
-
-  def _caller_method_sym caller_string
-    caller_string.match(/`(.*)'/)[1].to_sym
-  end
 
   def _exclusions
     _hasher.exclusions
---
+++ lib/last_call.rb
@@ -0,0 +1,5 @@
+module Kernel
+  def last_call
+    caller[1].match(/`(.*)'/)[1].to_sym
+  end
+end
