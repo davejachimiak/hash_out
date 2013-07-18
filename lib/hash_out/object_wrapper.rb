870f9c99bdaffaaa44e1ab282db088144dd00af8
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-18 19:10:23 -0400

    wrapper: inherit from struct; privatize extractions

--- lib/hash_out/object_wrapper.rb
+++ lib/hash_out/object_wrapper.rb
@@ -1,27 +1,29 @@
 require 'hash_out/excludable'
 
 module HashOut
-  class ObjectWrapper
-    def initialize object
-      @object = object
-    end
-
+  class ObjectWrapper < Struct.new :object
     def excludable
-      @excludable ||= begin
-                        dup_obj = @object.dup
-                        dup_obj.extend Excludable
-                        dup_obj
-                      end
+      @excludable ||= duplicated_object.extend Excludable
     end
 
     def public_methods_requiring_no_args
-      @object.public_methods(false).select do |m|
-        [-1, 0].include? @object.method(m).arity
+      object.public_methods(false).select do |method|
+        requires_no_args? method
       end
     end
 
     def method_value_pair method
-      [method, @object.send(method)]
+      [method, object.send(method)]
+    end
+
+    private
+
+    def duplicated_object
+      object.dup
+    end
+
+    def requires_no_args? method
+      [-1, 0].include? object.method(method).arity
     end
   end
 end
